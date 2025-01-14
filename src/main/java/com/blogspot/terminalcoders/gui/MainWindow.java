5281f3f19a263b688bff023aa3e3349c360a8fa5
Ajith KP <ajithkp560@gmail.com>
2025-01-14 17:25:51 +0530

    Add Menu Items and Actions

--- src/main/java/com/blogspot/terminalcoders/gui/MainWindow.java
+++ src/main/java/com/blogspot/terminalcoders/gui/MainWindow.java
@@ -3,7 +3,11 @@ package com.blogspot.terminalcoders.gui;
 import javax.swing.*;
 
 public class MainWindow extends JFrame {
+    MenuController menuController = new MenuController();
     public MainWindow(String title) {
         super(title);
+
+        // Add Menu to Window
+        this.setJMenuBar(menuController.getMainMenuBar());
     }
 }
--- src/main/java/com/blogspot/terminalcoders/gui/MenuController.java
+++ src/main/java/com/blogspot/terminalcoders/gui/MenuController.java
@@ -1,4 +1,47 @@
 package com.blogspot.terminalcoders.gui;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class MenuController {
+    private JMenuBar mainMenuBar;
+    private JMenu fileMenu;
+    private JMenuItem connectHostMenu, aboutMenu, exitMenu;
+
+    public MenuController() {
+        // Init Menu Bar
+        mainMenuBar = new JMenuBar();
+
+        // Init Main Menu
+        fileMenu = new JMenu("File");
+
+        // Init File - Submenu
+        connectHostMenu = new JMenuItem("Connect");
+        aboutMenu = new JMenuItem("About");
+        exitMenu = new JMenuItem("Exit");
+
+        // Add Menu Items
+        fileMenu.add(connectHostMenu);
+        fileMenu.add(aboutMenu);
+        fileMenu.addSeparator();
+        fileMenu.add(exitMenu);
+
+        // Add to Menu Bar
+        mainMenuBar.add(fileMenu);
+
+        // Add Close Action
+        exitMenu.addActionListener(new ExitActionListener());
+    }
+
+    public JMenuBar getMainMenuBar() {
+        return mainMenuBar;
+    }
+
+    class ExitActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.exit(0);
+        }
+    }
 }
